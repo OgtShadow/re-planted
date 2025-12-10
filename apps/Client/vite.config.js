5551c8398a3ee496b4286f99c83a595e6144021b
Piotr Szerłomski <pszerlomski@gmail.com>
2025-12-10 15:32:54 +0100

    add client port from env

--- apps/Client/vite.config.js
+++ apps/Client/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 5173,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 5173,
+      hmr: {
+        protocol: 'ws',
+        host: 'localhost',
+        port: parseInt(env.VITE_PORT) || 5173,
+      },
     },
-  },
+  }
 })
