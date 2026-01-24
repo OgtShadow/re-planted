e5ff996b7952fc2575195a38aa38cfa05c0d733a
Piotr Szerłomski <pszerlomski@gmail.com>
2026-01-24 14:05:40 +0100

    do some more styling

--- apps/Client/src/components/PlantProfile/PlantProfile.jsx
+++ apps/Client/src/components/PlantProfile/PlantProfile.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './PlantProfile.css';
 import PlantEditWindow from '../PlantEditWindow/PlantEditWindow';
+import StatusDot from '../StatusDot/StatusDot';
 
 function PlantProfile({plant}) {
     const [isEditing, setIsEditing] = useState(false);
@@ -8,9 +9,11 @@ function PlantProfile({plant}) {
 
 return (
     <div className="plant-profile">
+      <div className="plant-profile-header">
         <h2>{plant.name}</h2>
-        <p>Species: {plant.species}</p>
-        <p>Health Status: {plant.healthStatus}</p>
+        {plant.healthStatus === "Healthy" ? <StatusDot status="green" size="small" /> : plant.healthStatus === "Unhealthy" ? <StatusDot status="red" size="small" /> : <StatusDot status="gray" size="small" />} 
+      </div>
+        <p>Specie: {plant.species}</p>
         <button onClick={() => setIsEditing(true)}>Edit Plant</button>
         {isEditing && <PlantEditWindow plant={plant} onClose={(response) => { setIsEditing(false); setResponse(response); }} />}
           {console.log(response)}
