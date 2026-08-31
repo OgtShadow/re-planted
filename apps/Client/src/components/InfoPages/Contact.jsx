5e88bb154ba4322d851fd47437a9230cc795929b
Piotr Szerłomski <pszerlomski@gmail.com>
2026-08-31 21:30:27 +0200

    add basic contact

---
+++ apps/Client/src/components/InfoPages/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Contact.css'
+
+
+const Contact = () => {
+    return (
+        <div className="contact-container">
+            <div className="contact-content">
+            
+                <h1>Contact Us</h1>
+                <p>If you have any questions or need support, please reach out to us at:</p>
+                <p>Email: pszerlomski23@gmail.com</p>
+                <p>Phone: +48 730 019 004</p>
+            </div>
+            <div className="contact-icons">
+            </div>
+        </div>
+    )
+}
+
+export default Contact
